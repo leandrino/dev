2b3efec53237134fbb0839477a2e07756c6557c5
Developer <onmelancholyhill@tutanota.com>
2021-06-28 23:08:30 -0300

    title and divider blog post

--- application/ui/blog-post/blog-post.tsx
+++ application/ui/blog-post/blog-post.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import BasicLayout from "../Basic";
 import { Container } from "../../components/layout/container";
-import { Content } from './styles';
+import { Content, Title, Divider } from './styles';
 
 export function BlogPost(props: any) {
   return (
     <BasicLayout>
       <Container>
-        <h1>{props.blog.title}</h1>
+        <Title>{props.blog.title}</Title>
+        <Divider />
         <Content dangerouslySetInnerHTML={{ __html: props.blog.content }} />
       </Container>
     </BasicLayout>
--- application/ui/blog-post/styles.tsx
+++ application/ui/blog-post/styles.tsx
@@ -18,3 +18,18 @@ export const Content = styled.section`
     color: ${({ theme }: { theme: Theme }) => theme.title}
   }
 `
+
+export const Title = styled.h1`
+  margin: 20px;
+  color: ${({ theme }: { theme: Theme }) => theme.title}
+`
+
+export const Divider = styled.hr`
+  background: rgb(228,14,61);
+  background: linear-gradient(45deg, #e62b54 0%, #8f07b1 45%, #3672e2 75%);
+  border-radius: 10px;
+  border: none;
+  height: 8px;
+  margin: 0 20px;
+  width: 100px;
+`
